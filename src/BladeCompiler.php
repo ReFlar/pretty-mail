c51cd8a0ad8e6c0c4e0c001b970cd64c203ab0eb
Charlie <KyrneDev@users.noreply.github.com>
2021-03-11 07:02:44 +0000

    Apply fixes from StyleCI

--- src/BladeCompiler.php
+++ src/BladeCompiler.php
@@ -1,5 +1,15 @@
 <?php
 
+/*
+ * This file is part of fof/pretty-mail.
+ *
+ * Copyright (c) 2019 - 2021 FriendsOfFlarum.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ */
+
 namespace FoF\PrettyMail;
 
 use Symfony\Component\Debug\Exception\FatalThrowableError;
